59339aea4a4810b4f48cffb3e7458a269aefbaa9
원정호 <jhwon00@kaist.ac.kr>
2018-11-10 01:48:12 +0900

    일단 프로그램 구조를 좀 살펴봤고, 파이어베이스 데이터 가져오기 함수를 어떻게 짤지 고민을 조금 해 봤음.

--- src/firebase/emotion.js
+++ src/firebase/emotion.js
@@ -1,9 +1,18 @@
 import database from './database.js'
 
+// 임시 선언 구간. 나중에 정식으로 서버에서 id 받는 기능을 만들어야 할 듯.
+var id = 0xc0ffee
+// type는 어떻게 할까?
+//
+
 export function sendEmotion (type) {
   database.ref('/').set({
-    sender: '1',
+    sender: id,
     type: type,
-    timestamp: '2018-11-09T13:11:01'
+    timestamp: Date.now().toJSON()
   })
+}
+
+export function refresh () {
+  // 아마도 파이어베이스에서 전체 감정 목록을 받아오는 기능.
 }
