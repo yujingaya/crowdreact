2dccfc4822c4724be0f1b3780389230abc8307ae
YujinGaya <yujin.gaya@gmail.com>
2018-11-17 18:44:38 +0900

    Add animation to comment sending

--- src/components/EmotionComment.vue
+++ src/components/EmotionComment.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="emotion-comment">
     <div class="field">
-      <p class="control has-icons-right">
-        <input class="input" placeholder="What's happening?">
-        <span class="icon is-small is-right">
+      <p class="control">
+        <input 
+          v-model="message"
+          ref="input"
+          class="input is-rounded"
+          placeholder="What's happening?">
+        <transition
+          @before-enter="beforeEnter"
+          @after-enter="afterEnter">
+          <div
+            v-show="sending"
+            class="plane">
+            {{ message }}
+          </div>
+        </transition>
+        <span @click="send"
+          class="send-button-container">
           <i class="fas fa-paper-plane"></i>
         </span>
       </p>
@@ -13,11 +27,38 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      sending: false,
+      message: ''
+    }
+  },
+  methods: {
+    send () {
+      this.sending = true
+      setTimeout(() => {
+        this.sending = false
+      }, 600)
+    },
+    beforeEnter (el) {
+      const { top, left } = this.$refs.input.getBoundingClientRect()
+      el.style.position = 'fixed'
+      el.style.top = top + 6 + 'px'
+      el.style.left = left + 12 + 'px'
+      el.style.fontSize = '1rem'
+      el.style.opacity = '0.25'
+    },
+    afterEnter (el) {
+      el.style.top = '1rem'
+      el.style.left = 'calc(100% - 1.5rem)'
+      el.style.fontSize = '0.75rem'
+      el.style.opacity = '1'
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
 .emotion-comment {
   padding: 1rem 0.5rem;
   align-content: center;
@@ -25,8 +66,35 @@ export default {
 
 .emotion-comment input {
   font-size: 1rem;
-  padding: 0.3rem;
-  /* width: 96%; */
-  border-color:darkblue;
+  padding-left: 0.75rem;
+}
+
+.emotion-comment input:focus {
+  border: 1px solid gray;
+  box-shadow: none;
+}
+
+.control {
+  position: relative;
+}
+
+.send-button-container {
+  position: absolute;
+  top: 0rem;
+  right: 0rem;
+  padding: 0.2rem 0.5rem 0.2rem 0.4rem;
+  margin: 0.175rem;
+  border-radius: 1rem;
+  background-color: var(--theme-blue);
+
+  cursor: pointer;
+}
+
+.plane {
+  white-space: nowrap;
+  transition:
+    top 0.4s ease-out,
+    left 0.4s ease-in,
+    font-size 0.4s ease-in;
 }
 </style>
