737f97b7180ea54158712235b770e652d01a1fd8
YujinGaya <yujin.gaya@gmail.com>
2018-11-20 04:23:30 +0900

    Sync graph

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="graph scrollbar1" id="style-2">
     <div class="force-overflow">
-      <canvas id="planet-chart" width="400" height="400"></canvas>
+      <canvas id="chart" width="400" height="400"></canvas>
     </div>
   </div>
 
@@ -9,33 +9,105 @@
 
 <script>
 import Chart from 'chart.js'
-import planetChartData from '../chart-data.js'
+import { emotionsRef } from '@/firebase/emotion.js'
+
+// const period = 5 * 60 * 1000
+const period = 60 * 1000
 
 export default {
   data () {
     return {
-      planetChartData: planetChartData
+      chart: null,
+      callback: null,
+      start: null
     }
   },
   methods: {
-    createChart (chartId, chartData) {
-      const ctx = document.getElementById(chartId)
-      // eslint-disable-next-line no-new
-      new Chart(ctx, {
-        type: chartData.type,
-        data: chartData.data,
-        options: chartData.options
+    createChart () {
+      this.chart = new Chart('chart', {
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: 'angry',
+              borderColor: 'rgb(255, 154, 162)',
+              backgroundColor: 'rgba(255, 154, 162, 0.3)',
+              data: []
+            },
+            {
+              label: 'bored',
+              borderColor: 'rgb(178, 220, 247)',
+              backgroundColor: 'rgba(178, 220, 247, 0.3)',
+              data: []
+            },
+            {
+              label: 'curious',
+              borderColor: 'rgb(181, 234, 215)',
+              backgroundColor: 'rgba(181, 234, 215, 0.3)',
+              data: []
+            },
+            {
+              label: 'like',
+              borderColor: 'rgb(199, 206, 234)',
+              backgroundColor: 'rgba(199, 206, 234, 0.3)',
+              data: []
+            },
+            {
+              label: 'love',
+              borderColor: 'rgb(251, 226, 142)',
+              backgroundColor: 'rgba(251, 226, 142, 0.3)',
+              data: []
+            },
+            {
+              label: 'surprised',
+              borderColor: 'rgb(255, 218, 193)',
+              backgroundColor: 'rgba(255, 218, 193, 0.3)',
+              data: []
+            }
+          ]
+        }
       })
     }
   },
   mounted () {
-    this.createChart('planet-chart', this.planetChartData)
+    this.createChart()
+    this.callback = emotionsRef.on('child_added', emotion => {
+      const { timestamp, type, sender } = emotion.val()
+      const bucket = Math.floor((timestamp + period / 2) / period) * period
+      if (this.start === null) {
+        this.start = bucket
+      }
+
+      const currentLength = this.chart.data.labels.length
+      const bucketPosition = (bucket - this.start) / period
+      console.log(currentLength, bucketPosition)
+      for (let i = currentLength; i <= bucketPosition; i++) {
+        const labelMoment = new Date(this.start + (currentLength + i) * period)
+        const label = `${labelMoment.getHours()}:${(labelMoment.getMinutes() < 10 ? '0' : '') + labelMoment.getMinutes()}`
+        this.chart.data.labels.push(label)
+        this.chart.data.datasets.forEach(dataset => {
+          dataset.data.push(0)
+        })
+      }
+
+      this.chart.data.datasets.forEach(dataset => {
+        if (dataset.label === type) {
+          dataset.data[dataset.data.length - 1] += 1
+        }
+      })
+      this.chart.update()
+      console.log(this.chart.data)
+    })
+  },
+  unmount () {
+    emotionsRef.off('child_added', this.callback)
   }
 }
 </script>
 
 <style scoped>
-.scrollbar1
+/* .scrollbar1
 {
   margin-left: 30px;
   float: left;
@@ -68,6 +140,5 @@ export default {
   border-radius: 10px;
   -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
   background-color: pink;
-}
-
+} */
 </style>
--- src/firebase/emotion.js
+++ src/firebase/emotion.js
@@ -1,10 +1,17 @@
 import database from './database.js'
 
+// rehydrate 해도 좋을듯?
+function uuid (a) {
+  return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid)
+}
+
+const myId = uuid()
+
 export const emotionsRef = database.ref('/emotions')
 
 export function sendEmotion (type) {
   database.ref('/emotions').push({
-    sender: '1',
+    sender: myId,
     type: type,
     timestamp: Date.now()
   })
