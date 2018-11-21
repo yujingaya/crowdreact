b9a32948e4bd8aee9def045d9daf7d175e94b2f9
원정호 <wonjeongho@nclab.kaist.ac.kr>
2018-11-21 12:48:12 +0900

    일단 엄청 무식하게 쿠키 저장하는 방식으로 해결했음. 나중에 쿠키 더 쓸 거면 데이터 쓰는 방식 바꿔야 함.

--- src/firebase/emotion.js
+++ src/firebase/emotion.js
@@ -3,8 +3,8 @@ import database from './database.js'
 // rehydrate 해도 좋을듯?
 // 보낸 사람을 식별하기 위해서 랜덤한 문자열 생성.
 // 다만 새로고침할 때마다 값이 달라지니 localStorage나 쿠키로 저장해둘 수도 있을듯.
-function uuid (a) {
-  return a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, uuid)
+function uuid () {
+  return document.cookie ? document.cookie : (document.cookie = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a => (a ^ Math.random() * 16 >> a / 4).toString(16)))
 }
 
 const myId = uuid()
