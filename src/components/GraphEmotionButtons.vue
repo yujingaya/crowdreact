8f65e149ffdbff9dfd9b09534de10bc980b3053f
Jisu Han <jisuhan.it@gmail.com>
2018-11-22 13:24:41 +0900

    add item_count and sample value

--- src/components/GraphEmotionButtons.vue
+++ src/components/GraphEmotionButtons.vue
@@ -11,7 +11,14 @@
     <div v-for="item in items"
       :key="item.emotion" align="center">
       <img :src="item.emoji">
-      <div class="counts">100</div>
+      <!-- <div
+        v-for="emotion in emotions"
+        :key="emotion.type"
+        class="counts"
+        :class="{ 'my-counts': (item.emotion === emotion.type) }"
+        > {{ item.count }} 
+      </div> -->
+      <div class="counts">{{ item.count }} </div>
     </div>
   </div>
 </template>
@@ -21,12 +28,12 @@ export default {
   data () {
     return {
       items: [
-        { emotion: 'angry', emoji: require('@/assets/angry.png') },
-        { emotion: 'curious', emoji: require('@/assets/curious.png') },
-        { emotion: 'love', emoji: require('@/assets/love.png') },
-        { emotion: 'bored', emoji: require('@/assets/bored.png') },
-        { emotion: 'like', emoji: require('@/assets/like.png') },
-        { emotion: 'surprised', emoji: require('@/assets/surprised.png') }
+        { emotion: 'angry', emoji: require('@/assets/angry.png'), count: 3 },
+        { emotion: 'curious', emoji: require('@/assets/curious.png'), count: 7 },
+        { emotion: 'love', emoji: require('@/assets/love.png'), count: 5 },
+        { emotion: 'bored', emoji: require('@/assets/bored.png'), count: 40 },
+        { emotion: 'like', emoji: require('@/assets/like.png'), count: 1 },
+        { emotion: 'surprised', emoji: require('@/assets/surprised.png'), count: 5 }
       ]
     }
   }
